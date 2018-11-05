aaafa7da9f382eb0972f7391d99042a3d4b189f1
cice <cice@A8POVMac10.local>
2018-11-05 21:23:58 +0100

    first commit

---
+++ src/test/java/com/cice/springzuul/CiceSpringZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cice.springzuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiceSpringZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
